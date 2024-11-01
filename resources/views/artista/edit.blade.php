9aaa7f41e5ae69009103bab3720450ab7d1605c3
Keysi <kfajardop@miumg.edu.gt>
2024-10-31 21:53:38 -0600

    Creacion de CRUD para los artistas

---
+++ resources/views/artista/edit.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('template_title')
+    {{ __('Update') }} Artista
+@endsection
+
+@section('content')
+    <section class="content container-fluid">
+        <div class="">
+            <div class="col-md-12">
+
+                <div class="card card-default">
+                    <div class="card-header">
+                        <span class="card-title">{{ __('Update') }} Artista</span>
+                    </div>
+                    <div class="card-body bg-white">
+                        <form method="POST" action="{{ route('artistas.update', $artista->id) }}"  role="form" enctype="multipart/form-data">
+                            {{ method_field('PATCH') }}
+                            @csrf
+
+                            @include('artista.form')
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
